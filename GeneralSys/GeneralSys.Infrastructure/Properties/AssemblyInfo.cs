2e267bf3a948117c7099e362e038d8dd6c057085
miracle98 <miracle98@yeah.net>
2016-04-26 17:34:25 +0800

    cleanup code

--- GeneralSys/GeneralSys.Infrastructure/Properties/AssemblyInfo.cs
+++ GeneralSys/GeneralSys.Infrastructure/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
+
 [assembly: AssemblyTitle("GeneralSys.Infrastructure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +17,11 @@ using System.Runtime.InteropServices;
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
 //请将此类型的 ComVisible 特性设置为 true。
+
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+
 [assembly: Guid("0896eccc-8d3a-4aed-9e0d-a8b9ed4514de")]
 
 // 程序集的版本信息由下列四个值组成: 
@@ -32,5 +34,6 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
